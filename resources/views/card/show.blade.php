70c24b007109ce6895366bbb6fd912bf8dbaa144
Tatsu <tatsuhiroabe0127@gmail.com>
2020-05-31 19:15:29 +0900

    modified card/show.blade.php

--- resources/views/card/show.blade.php
+++ resources/views/card/show.blade.php
@@ -3,7 +3,7 @@
 <diV class="h3">リスト名：{{ $listing->title }}</diV>
 <div class="h3">タスク名：{{ $card->title }}</div>
 <div class="h3">概要：{{ $card->content }}</div>
-<div class="h3">投稿日時：{{ $card->created_at }}</div>
+<div class="h3">投稿日時：{{ $card->updated_at }}</div>
 @canany(['update', 'delete'], $listing)
 <a class="btn btn-success" href="/listing/{{ $listing->id }}/card/{{ $card->id }}/edit">編集する</a>
 <a class="btn btn-danger" href="/listing/{{ $listing->id }}/card/{{ $card->id }}/delete">削除する</a>
